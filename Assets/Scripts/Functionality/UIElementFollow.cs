55be57fbbc846c40bc577b0c44130133b15ca919
swerum <sarah.elisabeth.werum@gmail.com>
2020-12-07 14:14:14 +0100

    restructured folders

---
+++ Assets/Scripts/Functionality/UIElementFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIElementFollow : MonoBehaviour
+{
+    [SerializeField] Transform follow = null;
+    [SerializeField] Vector2 offset = Vector2.zero;
+    Camera main;
+
+    private void Start()
+    {
+        main = Camera.main;
+        GameObject canvas = GameObject.FindGameObjectWithTag("Canvas");
+        transform.SetParent(canvas.transform);
+    }
+
+    void Update()
+    {
+        Vector3 uiPos = main.WorldToScreenPoint(follow.position);
+        Vector2 newPosition = new Vector2(uiPos.x + offset.x, uiPos.y + offset.y);
+        transform.position = newPosition;
+    }
+}
